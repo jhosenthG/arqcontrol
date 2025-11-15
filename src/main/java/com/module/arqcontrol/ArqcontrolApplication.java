70d332854b5febc46716b3e6398c4289a4c98335
jhosenthg <jhosenth@gmail.com>
2025-11-15 15:43:53 -0400

    Initial commit

---
+++ src/main/java/com/module/arqcontrol/ArqcontrolApplication.java
@@ -0,0 +1,13 @@
+package com.module.arqcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArqcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArqcontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/module/arqcontrol/ArqcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.module.arqcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArqcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
